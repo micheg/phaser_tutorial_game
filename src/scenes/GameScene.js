4f7812d33a9611a25db36d2d124f45aea2044a3a
michelangelo giacomelli <michelangelog@gmail.com>
2021-05-29 12:19:25 +0200

    fix

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -10,16 +10,10 @@ export default class GameScene extends Phaser.Scene
 
     preload()
     {
-        /*this.load.image('sky', 'img/sky.png');
-        this.load.image('ground', 'img/platform.png');
-        this.load.image('star', 'img/star.png');
-        this.load.image('bomb', 'img/bomb.png');*/
-
         this.load.spritesheet('dude', 'assets/dude.png',
         {
             frameWidth: 32, frameHeight: 48
         });
-
         this.load.bitmapFont('pixelFont', 'font/font.png', 'font/font.xml');
     }
 
@@ -29,9 +23,35 @@ export default class GameScene extends Phaser.Scene
         this.add.image(CENTER_X, CENTER_Y + 50, 'star');
         this.add.image(CENTER_X, CENTER_Y - 60, 'star');
         this.add.rectangle(CENTER_X, HEIGHT - 10, WIDTH, 20, 0x000000);
-        this.add.bitmapText(4, HEIGHT - 17, 'pixelFont', 'About', 20);
-        this.add.bitmapText(200, HEIGHT - 17, 'pixelFont', 'Rulez', 20);
-        let start_tet = this.add.bitmapText(CENTER_X, CENTER_Y, 'pixelFont', 'START', 40);
+        this.add.bitmapText(4, HEIGHT - 17, 'pixelFont', 'Menu', 20);
+        let start_tet = this.add.bitmapText(CENTER_X, CENTER_Y, 'pixelFont', 'GAME', 40);
         start_tet.setOrigin(0.5, 0.5);
+
+        // kaios softkeys
+        this.input.keyboard.on( 'keydown', (e) =>
+        {
+            switch (e.key)
+            {
+                case 'SoftLeft':
+                    this.scene.pause();
+                    this.scene.start('start-scene');
+                    break;
+            }
+        });
+        this.leftButton = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.Q);
+    }
+
+    uodate_keybind()
+    {
+
+        if (this.leftButton.isDown)
+        {
+            this.scene.start('start-scene');
+        }
+    }
+
+    uodate()
+    {
+        this.uodate_keybind();
     }
 }
